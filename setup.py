9c9122512182e336389978ad12391f52c7443e7d
John Sturdy <jcg.sturdy@gmail.com>
2026-06-25 22:24:01 +0100

    new version with purge of ancient print statements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="coimealta",
-    version="0.0.14",
+    version="0.0.15",
     description="A keeper for information about inventory and contacts.",
     long_description="""
     A keeper for information about inventory and contacts.
